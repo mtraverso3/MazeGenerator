d048ea3c4818387c29e1432067ec3a556e13cbed
Marcos Traverso <marcostraverso2003@gmail.com>
2020-03-17 23:09:10 -0700

    Added to findNext()

--- src/Maze.java
+++ src/Maze.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Maze {
     private final Cell[][] maze;
     private Coordinate current;
+    private final int width;
+    private final int height;
 
     Maze(int width, int height, Coordinate start) {
+        this.width = width;
+        this.height = height;
         this.current = start;
         this.maze = new Cell[width][height];
 
@@ -23,11 +29,23 @@ public class Maze {
     }
 
     private Direction findNext() {   //TODO implement findNext
-
+        double[] probabilites = new double[4];
+        Arrays.fill(probabilites, 0.25);
 
         if (current.getRow() == 0) {
-
+            probabilites[0] = 0;
         }
+        if (current.getColumn() == 0) {
+            probabilites[2] = 0;
+        }
+        if (current.getRow() == width - 1) {
+            probabilites[1] = 0;
+        }
+        if (current.getColumn() == width - 1) {
+            probabilites[3] = 0;
+        }
+
+
         return Direction.NORTH;
     }
 
